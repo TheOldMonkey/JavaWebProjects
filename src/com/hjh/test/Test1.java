face87972f6b83aff32cfaceb9088044e38366f2
TheOldMonkey <1058623794@qq.com>
2018-02-01 14:26:22 +0800

    test

---
+++ src/com/hjh/test/Test1.java
@@ -0,0 +1,19 @@
+package com.hjh.test;
+
+public class Test1 {
+	
+	public static Test1 test1 = new Test1("yu");
+	
+	public Test1(String string) {
+		System.out.println(string);
+	}
+	
+	public static void main(String[] args) {
+		for(int i = 1, j = 10; i + j < 25; i++, j++) {
+//			System.out.println("i=" + i + "; j=" + j);
+		}
+		
+//		System.out.println(Integer.toBinaryString(10));
+	}
+
+}
